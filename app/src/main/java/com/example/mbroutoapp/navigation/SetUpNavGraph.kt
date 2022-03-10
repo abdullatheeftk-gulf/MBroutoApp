79a1721019735fc726b9f8ea79433d6dc09d642c
abdullatheeftk-gulf <68467790+abdullatheeftk-gulf@users.noreply.github.com>
2022-03-10 22:11:37 +0400

    v35 & 36 finished

--- app/src/main/java/com/example/mbroutoapp/navigation/SetUpNavGraph.kt
+++ app/src/main/java/com/example/mbroutoapp/navigation/SetUpNavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.mbroutoapp.presentation.screens.splash.SplashScreen
 import com.example.mbroutoapp.util.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
@@ -17,7 +18,7 @@ fun SetUpNavGraph(
             startDestination = Screen.Splash.route
         ){
             composable(route = Screen.Splash.route){
-
+                SplashScreen(navController = navController)
             }
             composable(route = Screen.Welcome.route){
 
---
+++ app/src/main/java/com/example/mbroutoapp/presentation/screens/splash/SplashScreen.kt
@@ -0,0 +1,94 @@
+package com.example.mbroutoapp.presentation.screens.splash
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.Animatable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import com.example.mbroutoapp.R
+import com.example.mbroutoapp.ui.theme.Purple500
+import com.example.mbroutoapp.ui.theme.Purple700
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.ui.draw.rotate
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+
+    val degree =  remember {
+      Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true ){
+        degree.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+    }
+    Splash(degrees = degree.value)
+}
+
+@Composable
+fun Splash(degrees:Float) {
+    if (isSystemInDarkTheme()) {
+        Box(
+            modifier = Modifier
+                .background(Color.Black)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                modifier = Modifier.rotate(degrees),
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(
+                    R.string.app_logo
+                )
+            )
+        }
+    } else {
+        Box(
+            modifier = Modifier
+                .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                modifier = Modifier.rotate(degrees),
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(
+                    R.string.app_logo
+                )
+            )
+        }
+    }
+
+
+}
+
+/*
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    Splash()
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun SplashScreenPreviewNight() {
+    Splash()
+}*/
