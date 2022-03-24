0255c09a64a22c80f05fdcde3d023500b5bde5a5
abdullatheeftk-gulf <68467790+abdullatheeftk-gulf@users.noreply.github.com>
2022-03-24 21:16:09 +0400

    v52 filled star design finished

--- app/src/main/java/com/example/mbroutoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/mbroutoapp/ui/theme/Color.kt
@@ -11,6 +11,8 @@ val Teal200 = Color(0xFF03DAC5)
 
 val LightGray = Color(0xFFD8D8D8)
 val DarkGray = Color(0xFF2A2A2A)
+val StarColor = Color(0xFFFFC94D)
+
 
 
 val Colors.welcomeScreenBackGroundColor
