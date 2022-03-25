71547703eb51defd2ef753628e5c26dfe9d80317
abdullatheeftk-gulf <68467790+abdullatheeftk-gulf@users.noreply.github.com>
2022-03-25 08:21:26 +0400

    v55 filled star design finished

--- app/src/main/java/com/example/mbroutoapp/presentation/components/RatingWidget.kt
+++ app/src/main/java/com/example/mbroutoapp/presentation/components/RatingWidget.kt
@@ -1,10 +1,12 @@
 package com.example.mbroutoapp.presentation.components
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
@@ -15,17 +17,23 @@ import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.vector.PathParser
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.mbroutoapp.R
+import com.example.mbroutoapp.ui.theme.EXTRA_SMALL_PADDING
 import com.example.mbroutoapp.ui.theme.LightGray
 import com.example.mbroutoapp.ui.theme.StarColor
+
+private const val TAG = "RatingWidget"
 
 @Composable
 fun RatingWidget(
     modifier: Modifier,
     rating: Double,
-    scaleFactor: Float = 3f
+    scaleFactor: Float = 3f,
+    spaceBetweenStars: Dp = EXTRA_SMALL_PADDING
 ) {
+    val result = calculateStars(rating = rating)
 
     val starPathString = stringResource(id = R.string.star_path)
     val starPath = remember {
@@ -36,11 +44,38 @@ fun RatingWidget(
         starPath.getBounds()
     }
 
-    FilledStar(
-        starPath = starPath,
-        starPathBounds = starPathBounds,
-        scaleFactor = scaleFactor
-    )
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(space = spaceBetweenStars)
+    ) {
+        result["filledStars"]?.let {
+            repeat(it){
+                FilledStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+        result["halfFilledStars"]?.let {
+            repeat(it){
+                HalfFilledStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+        result["emptyStars"]?.let {
+            repeat(it){
+                EmptyStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+    }
 }
 
 @Composable
@@ -51,7 +86,7 @@ fun FilledStar(
 ) {
 
     Canvas(modifier = Modifier.size(24.dp)) {
-        val canvasSize = this.size
+        val canvasSize = size
 
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
@@ -78,7 +113,7 @@ fun HalfFilledStar(
     scaleFactor: Float
 ) {
     Canvas(modifier = Modifier.size(24.dp)) {
-        val canvasSize = this.size
+        val canvasSize = size
 
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
@@ -112,7 +147,7 @@ fun EmptyStar(
     scaleFactor: Float
 ) {
     Canvas(modifier = Modifier.size(24.dp)) {
-        val canvasSize = this.size
+        val canvasSize = size
 
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
@@ -128,6 +163,55 @@ fun EmptyStar(
             }
         }
     }
+}
+
+@Composable
+fun calculateStars(rating: Double): Map<String, Int> {
+    val maxStars by remember {
+        mutableStateOf(5)
+    }
+    var filledStars by remember {
+        mutableStateOf(0)
+    }
+    var halfFilledStars by remember {
+        mutableStateOf(0)
+    }
+    var emptyStars by remember {
+        mutableStateOf(0)
+    }
+    LaunchedEffect(key1 = rating) {
+        val (firstNumber, lastNumber) = rating.toString()
+            .split(".")
+            .map {
+                it.toInt()
+            }
+        if (firstNumber in 0..5 && lastNumber in 0..9) {
+            filledStars = firstNumber
+            if (lastNumber in 1..5) {
+                halfFilledStars++
+            }
+            if (lastNumber in 6..9) {
+                filledStars++
+            }
+            if (firstNumber == 5 && lastNumber > 0) {
+                emptyStars = 5
+                filledStars = 0
+                halfFilledStars = 0
+            }
+
+        } else {
+            Log.d(TAG, "CalculateStars: invalid rating number ")
+        }
+
+    }
+    emptyStars = maxStars - (filledStars + halfFilledStars)
+
+    return mapOf(
+        "filledStars" to filledStars,
+        "halfFilledStars" to halfFilledStars,
+        "emptyStars" to emptyStars
+    )
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/mbroutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/mbroutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,13 +1,17 @@
 package com.example.mbroutoapp.presentation.screens.home
 
 import android.util.Log
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.mbroutoapp.domain.model.Hero
+import com.example.mbroutoapp.presentation.components.RatingWidget
+import com.example.mbroutoapp.ui.theme.LARGE_PADDING
 
 private const val TAG = "HomeScreen"
 @Composable
@@ -22,6 +26,9 @@ fun HomeScreen(
 
         }
     }) {
-
+            RatingWidget(
+                modifier = Modifier.padding(all = LARGE_PADDING),
+                rating = 0.0
+            )
     }
 }
